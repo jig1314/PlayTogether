c6f51e832a3b4b937254712d2163923b9e5e2682
jgambl24 <jgambl24@students.kennesaw.edu>
2021-03-02 01:11:47 -0500

    Initial Scaffolding Results with Minor modifications to fix errors

---
+++ PlayTogether/Server/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PlayTogether.Server.Data;
+using PlayTogether.Server.Models;
+
+[assembly: HostingStartup(typeof(PlayTogether.Server.Areas.Identity.IdentityHostingStartup))]
+namespace PlayTogether.Server.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
