cbbca7232b820f65bc297db20559c3f163650305
jgambl24 <jgambl24@students.kennesaw.edu>
2021-03-16 21:22:52 -0400

    Removed strict validation on login page. Only valids on submit.
    Related Work Items: #32

--- PlayTogether/Client/ViewModels/LoginViewModel.cs
+++ PlayTogether/Client/ViewModels/LoginViewModel.cs
@@ -10,11 +10,9 @@ namespace PlayTogether.Client.ViewModels
     {
         [Required]
         [Display(Name = "Username")]
-        [RegularExpression("^(?=[a-zA-Z0-9._]{6,20}$)(?!.*[_.]{2})[^_.].*[^_.]$", ErrorMessage = "Please enter a valid username.")]
         public string UserName { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
