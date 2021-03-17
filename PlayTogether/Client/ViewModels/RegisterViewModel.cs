3e473f213720501a97ffdc7188f86294c29c5f66
jgambl24 <jgambl24@students.kennesaw.edu>
2021-03-16 23:06:30 -0400

    Implemented Fluent Validation and fixedstrange validation behavior on the page

--- PlayTogether/Client/ViewModels/RegisterViewModel.cs
+++ PlayTogether/Client/ViewModels/RegisterViewModel.cs
@@ -1,58 +1,110 @@
-﻿using PlayTogether.Shared.Models;
+﻿using FluentValidation;
+using PlayTogether.Shared.Models;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
 
 namespace PlayTogether.Client.ViewModels
 {
     public class RegisterViewModel
     {
-        [Required]
-        [DataType(DataType.Text)]
-        [Display(Name = "First Name")]
-        [RegularExpression("^([A-Z][a-zA-Z]{1,})$", ErrorMessage = "Please enter a valid first name.")]
         public string FirstName { get; set; }
 
-        [Required]
-        [DataType(DataType.Text)]
-        [Display(Name = "Last Name")]
-        [RegularExpression("^([A-Z][a-zA-Z]{1,})$", ErrorMessage = "Please enter a valid last name.")]
         public string LastName { get; set; }
 
-        [Required]
-        [DataType(DataType.Text)]
-        [EmailAddress]
-        [Display(Name = "Email Address")]
         public string Email { get; set; }
 
-        [Required]
-        [DataType(DataType.Text)]
-        [Display(Name = "Username")]
-        [RegularExpression("^(?=[a-zA-Z0-9._]{6,20}$)(?!.*[_.]{2})[^_.].*[^_.]$", ErrorMessage = "Please enter a valid username.")]
         public string UserName { get; set; }
 
-        [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
-        [DataType(DataType.Password)]
-        [Display(Name = "Password")]
         public string Password { get; set; }
 
-        [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
-        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
 
-        [Required(ErrorMessage = "Please enter your Date of Birth.")]
-        [DataType(DataType.Date)]
-        [Display(Name = "Date of Birth")]
         public DateTime? DateOfBirth { get; set; }
 
-        [Required(ErrorMessage = "Please select your Country of Residence.")]
-        [Display(Name = "Country of Residence")]
         public int? CountryOfResidenceId { get; set; }
 
-        [Required(ErrorMessage = "Please select your Gender.")]
-        [Display(Name = "Gender")]
         public int? GenderId { get; set; }
+    }
+
+    public class RegisterValidator : AbstractValidator<RegisterViewModel>
+    {
+        public RegisterValidator()
+        {
+            CascadeMode = CascadeMode.StopOnFirstFailure;
+
+            RuleFor(x => x.FirstName)
+                .NotEmpty()
+                .WithMessage("Please enter a valid first name.")
+                .Matches("^([A-Z][a-zA-Z]{1,})$")
+                .WithMessage("Please enter a valid first name.");
+
+            RuleFor(x => x.LastName)
+                .NotEmpty()
+                .WithMessage("Please enter a valid last name.")
+                .Matches("^([A-Z][a-zA-Z]{1,})$")
+                .WithMessage("Please enter a valid last name.");
+
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                .WithMessage("Please enter valid email address")
+                .EmailAddress()
+                .WithMessage("Please enter valid email address");
+
+            RuleFor(x => x.UserName)
+                .NotEmpty()
+                .WithMessage("Please enter a valid username.")
+                .Matches("^(?=[a-zA-Z0-9._]{6,20}$)(?!.*[_.]{2})[^_.].*[^_.]$")
+                .WithMessage("Please enter a valid username.");
+
+            RuleFor(x => x.Password)
+                .NotEmpty().WithMessage("Please enter password.")
+                .MinimumLength(6).WithMessage("The password must be at least 6 characters long.")
+                .MaximumLength(100).WithMessage("The password must be a maximum of 100 characters long.")
+                .Custom((password, context) =>
+                {
+                    var hasNumber = new Regex(@"[0-9]+");
+                    var hasLowerChar = new Regex(@"[a-z]+");
+                    var hasUpperChar = new Regex(@"[A-Z]+");
+                    var hasMinimum6Chars = new Regex(@".{6,}");
+                    var hasSymbols = new Regex(@"[!@#$%^&*()_+=\[{\]};:<>|./?,-]");
+
+                    var isValid = hasNumber.IsMatch(password) && hasLowerChar.IsMatch(password) && hasUpperChar.IsMatch(password) && hasMinimum6Chars.IsMatch(password) && hasSymbols.IsMatch(password);
+                    if (!isValid)
+                    {
+                        context.AddFailure("Please enter a valid password.");
+                    }
+                });
+
+            RuleFor(x => x.ConfirmPassword)
+                .NotEmpty().WithMessage("Please re-enter password.")
+                .Equal(x => x.Password).WithMessage("The password and confirmation password do not match.");
+
+            RuleFor(x => x.DateOfBirth).Must(ValidDate)
+                .WithMessage("Please enter your Date of Birth.");
+
+            RuleFor(x => x.CountryOfResidenceId)
+                .NotEmpty()
+                .WithMessage("Please select your Country of Residence.")
+                .GreaterThan(0)
+                .WithMessage("Please select your Country of Residence.");
+
+            RuleFor(x => x.GenderId)
+                .NotEmpty()
+                .WithMessage("Please select your Gender.")
+                .GreaterThan(0)
+                .WithMessage("Please select your Gender.");
+        }
+
+        private bool ValidDate(DateTime? date)
+        {
+            if (date.HasValue)
+            {
+                return DateTime.Now.Year - date.Value.Year < 150 && DateTime.Now.Date > date.Value.Date;
+            }
+
+            return false;
+        }
     }
 }
