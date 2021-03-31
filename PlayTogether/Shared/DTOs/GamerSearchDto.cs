c1bca341a63ee28e27ff34b35ba932f827fcb866
Jonathan Gamble <jgambl24@students.kennesaw.edu>
2021-03-31 01:23:24 -0400

    Added API calls and models for gamer search logic

---
+++ PlayTogether/Shared/DTOs/GamerSearchDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlayTogether.Shared.DTOs
+{
+    public class GamerSearchDto
+    {
+        public string SearchCriteria { get; set; }
+    }
+}
