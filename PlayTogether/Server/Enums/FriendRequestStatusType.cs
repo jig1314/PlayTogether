ba18e9c127a1918abdd196284cb83f6e959d6f6e
Jonathan Gamble <jgambl24@students.kennesaw.edu>
2021-04-02 14:09:42 -0400

    Added Friend and Friend Request Initial Schema

---
+++ PlayTogether/Server/Enums/FriendRequestStatusType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlayTogether.Server.Enums
+{
+    public enum FriendRequestStatusType
+    {
+        Sent = 1,
+        Accepted = 2,
+        Rejected = 3,
+        Cancelled = 4
+    }
+}
---
+++ PlayTogether/Server/Models/FriendRequest.cs
@@ -0,0 +1,30 @@
+﻿using PlayTogether.Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlayTogether.Server.Models
+{
+    public class FriendRequest
+    {
+        [Key]
+        public long Id { get; set; }
+
+        [Required]
+        public string FromUserId { get; set; }
+
+        public ApplicationUser FromUser { get; set; }
+
+        [Required]
+        public string ToUserId { get; set; }
+
+        public ApplicationUser ToUser { get; set; }
+
+        [Required]
+        public int FriendRequestStatusId { get; set; }
+
+        public FriendRequestStatusType FriendRequestStatus { get; set; }
+    }
+}
