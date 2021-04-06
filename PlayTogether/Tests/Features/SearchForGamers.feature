7e3f0f8d30d4d04089e312a02fd024b72658cfbd
Jonathan Gamble <jgambl24@students.kennesaw.edu>
2021-04-05 23:19:39 -0400

    Added BDD Test for Searching for Gamers. Added more unit tests to Register Validator.

---
+++ PlayTogether/Tests/Features/SearchForGamers.feature
@@ -0,0 +1,14 @@
+﻿Feature: Search For Gamers
+	As an user, I want to have the ability to search for other users based on their profile information so that I can view their profile.
+
+@BDD
+Scenario: An user with looking for other users
+	Given I have created a profile
+		| FirstName | LastName | Email             | Username | Password  | ConfirmPassword | Gender | CountryofResidence | DateofBirth |
+		| Test      | Name     | testing@gmail.com | testName | NotR3@Lpw | NotR3@Lpw       | Male   | United States      | 01/01/2000  |
+	And I have logged in
+	When I am on the gamer search page
+	Then I can filter for gamers by information based on their profile 
+		| SearchCriteria |
+		| gmail          |
+	And I will receive feedback
---
+++ PlayTogether/Tests/Pages/SearchForGamersPage.cs
@@ -0,0 +1,54 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
+
+namespace PlayTogether.Tests.Pages
+{
+    public class SearchForGamersPage
+    {
+        public IWebDriver WebDriver { get; }
+
+        public IWebElement SearchForGamersLink => WebDriver.FindElement(By.LinkText("Search For Gamers"));
+
+        public IWebElement GamerSearchBar => WebDriver.FindElement(By.Id("gamerSearchBar"));
+
+
+        public SearchForGamersPage(IWebDriver webDriver)
+        {
+            WebDriver = webDriver;
+        }
+
+        public bool SearchForGamersLinkVisible()
+        {
+            return SearchForGamersLink.Displayed;
+        }
+
+        public void ClickSearchForGamers()
+        {
+            SearchForGamersLink.Click();
+        }
+
+        public void SearchWithCriteria(Table searchCriteria)
+        {
+            dynamic search = searchCriteria.CreateDynamicInstance();
+            GamerSearchBar.SendKeys(search.SearchCriteria);
+            GamerSearchBar.SendKeys("\n");
+        }
+
+        public bool DidReceivedFeedback()
+        {
+            var receivedResult = WebDriver.FindElements(By.ClassName("col-lg-4"))?.Count > 0;
+
+            if (receivedResult)
+                return receivedResult;
+
+            var receivedNoResultsNotice = WebDriver.FindElement(By.Id("noResultsNotice")).Displayed;
+            return receivedNoResultsNotice;
+        }
+    }
+}
