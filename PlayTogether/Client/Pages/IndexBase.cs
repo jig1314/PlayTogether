725192a8fb79d6241c252778d6141f5afafed2d8
Jonathan Gamble <jgambl24@students.kennesaw.edu>
2021-04-20 23:47:16 -0400

    Change to Index page to test the service worker on test

---
+++ PlayTogether/Client/Pages/IndexBase.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlayTogether.Client.Pages
+{
+    public class IndexBase : ComponentBase
+    {
+        [CascadingParameter]
+        public Task<AuthenticationState> AuthenticationStateTask { get; set; }
+
+        public AuthenticationState AuthenticationState { get; set; }
+
+        public bool IsAuthenticated { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            AuthenticationState = await AuthenticationStateTask;
+            IsAuthenticated = AuthenticationState.User.Identity.IsAuthenticated;
+        }
+
+    }
+}
